d45579da8090d14a4198b4c8d25dc0bb41fed330
TLRKiliann <philogenie@protonmail.com>
2021-12-15 22:10:28 +0100

    transform color rotate translate text

--- html-css-js/second/index.js
+++ html-css-js/second/index.js
@@ -1,10 +1,12 @@
 // To change color of circle and change bg-color.
 
 const chgColor = document.getElementById("titlebox");
-const chgPosition = document.getElementById("titlebox");
+const chgPosition = document.getElementById("rotate-me");
 
 chgColor.addEventListener("click", () => {
   chgColor.classList.toggle("tochangecolor");
-  chgPosition.classList.toggle("position");
 });
 
+chgPosition.addEventListener("click", () => {
+  chgPosition.classList.toggle("position");
+});
