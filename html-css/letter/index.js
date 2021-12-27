20777d902d78468dc8f5561cf1c5f944de7e9bd0
TLRKiliann <philogenie@protonmail.com>
2021-12-27 17:45:38 +0100

    new folder to rotate letters

---
+++ html-css/letter/index.js
@@ -0,0 +1,7 @@
+
+const actionImg = document.getElementById("img-action");
+const circleImg = document.getElementById("mycircle");
+
+actionImg.addEventListener("click", () => {
+    document.body.classList.toggle("circle-animated")
+});
