40de64d8a23a5f0759a6f4da59add9221397912d
TLRKiliann <philogenie@protonmail.com>
2022-01-14 19:05:32 +0100

    model_projetc3 improved

--- html-css-js/model_project3/index.js
+++ html-css-js/model_project3/index.js
@@ -1,5 +1,14 @@
 //Button action
 
+/*
+Catch only first article
+const p4 = document.querySelector("body > article");
+querySelectorAll()
+with CSS class
+with class in HTML
+*/
+
+//Call of id buttons
 const btn1 = document.getElementById("btn-1");
 const btn2 = document.getElementById("btn-2");
 const btn3 = document.getElementById("btn-3");
@@ -9,17 +18,12 @@ const btn6 = document.getElementById("btn-6");
 const btn7 = document.getElementById("btn-7");
 const btn8 = document.getElementById("btn-8");
 const btn9 = document.getElementById("btn-9");
+const btn10 = document.getElementById("btn-10");
 
+//Call for action
 const p1 = document.querySelector("p");
 const p2 = document.querySelector(".btn-style2");
 const p3 = document.querySelector(".btn-style3");
-/*
-Catch only first article
-const p4 = document.querySelector("body > article");
-querySelectorAll()
-with CSS class
-with class in HTML
-*/
 const p4 = document.getElementById("art-one");
 const p5 = document.getElementById("art-two");
 const p6 = document.getElementById("art-three");
@@ -46,14 +50,38 @@ btn3.addEventListener("click", () => {
     p3.style.borderRadius = "10px";
 });
 
+let counter = 0;
 btn4.addEventListener("click", () => {
-    p4.style.color = "red";
-    p5.style.color = "red";
-    p6.style.color = "red";
+    counter += 1;
+    if (counter == 1) {
+        p4.style.color = "orange";
+        p5.style.color = "orange";
+        p6.style.color = "orange";
+    }
+    else if (counter == 2) {
+        p4.style.color = "navy";
+        p5.style.color = "navy";
+        p6.style.color = "navy";
+        counter = 0;
+    }
+    else {
+        counter = 0;
+    }
 });
 
+let scount = 0;
 btn5.addEventListener("click", () => {
-    p7.style.background = "orange";
+    scount += 1;
+    if (scount == 1) {
+        p7.style.background = "orange";
+    }
+    else if (scount == 2) {
+        document.body.style.background = "lightblue";
+        scount = 0;
+    }
+    else {
+        scount = 0;
+    }
 });
 
 btn6.addEventListener("click", () => {
@@ -70,15 +98,19 @@ btn8.addEventListener("click", () => {
 
 let count = 0;
 btn9.addEventListener("click", () => {
-    p11.innerHTML = `Button Style 9`;
+    p11.innerHTML = `Well done !`;
     count += 1;
     if (count < 2) {
         p11.innerHTML += `<p>Hello !!!</p><p>That's funny</p><p>to test!</p>`;
     } else {
-        p11.innerHTML = `Well done !`
+        p11.innerHTML = `Button Style 9`
         count = 0;
     }
 });
+
+btn10.addEventListener("click", () => {
+    alert("You've call an alert !");
+})
 
 window.addEventListener("load", () => {
   console.log("Document LOADED !");
