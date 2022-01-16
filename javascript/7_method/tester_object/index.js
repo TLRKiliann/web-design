6ae6a3d0169b6e18e459f9414f0be727a2df4e89
TLRKiliann <philogenie@protonmail.com>
2022-01-16 15:05:45 +0100

    splice and slice method added

--- javascript/7_method/tester_object/index.js
+++ javascript/7_method/tester_object/index.js
@@ -1,2 +1,41 @@
 //Tester Object Method
+//slice() and splice()
+
+/*
+Slice and Splice
+You are given two arrays and an index.
+
+Copy each element of the first array into 
+the second array, in order.
+
+Begin inserting elements at index n of 
+the second array.
+
+Return the resulting array. The input arrays 
+should remain the same after the function runs.
+*/
+
+/*
+On vous donne deux tableaux et un index.
+
+Copiez chaque élément du premier tableau dans 
+le second tableau, dans l'ordre.
+
+Commencez à insérer les éléments à l'indice n 
+du second tableau.
+
+Retournez le tableau résultant. Les tableaux 
+d'entrée doivent rester les mêmes après 
+l'exécution de la fonction.
+*/
+
+function frankenSplice(arr1, arr2, n) {
+  let newArr = arr2.slice()
+  console.log(newArr)
+  newArr.splice(n, 0, ...arr1)
+  console.log(newArr)
+  return newArr
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
 
